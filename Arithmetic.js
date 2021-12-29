cbe51070bd5fe3968d4f866238b277f6e964b711
centino001 <80419369+centino001@users.noreply.github.com>
2021-12-29 15:50:30 +0100

    final for arithmetic

--- Arithmetic.js
+++ Arithmetic.js
@@ -1,11 +1,39 @@
 //Hello, this file shows how to get around with Arithmetic operators
-//these operators are; '+' '-' '*' '/' '%' 
+//these operators are; '+' '-' '*' '/' '%' '**' '++' '--'
 //(addition, subtraction, multiplication, division and modulus division respectively)
 
-let x=3;//let is used to declare varaibles in javascript
-let y=16;
+
+
+//please read the comments carefully as it will guide you through this tutorial
+
+
+let x=16;//let is used to declare varaibles in javascript
+let y=3;
 
 //you could also declare the 2 variables using a single let keyword:::::
 //let x=4, y=15; :::::uncomment this line and test the code to be sure it's the same as the former
 
 console.log(x/y);
+console.log(x-y);
+console.log(x%y);
+console.log(x*y);
+console.log(x+y);
+console.log(x**y);//this here takes the value of x and exponents the value of y concatenating it to x: something like(x^y)
+
+
+console.log(++x);// this increments the value of x before displaying it on the console
+
+
+//for lines 19 and 20:::to see results, comment all the console.log codes and run lines 19 and 20{
+console.log(x++);
+console.log(x);
+//the above 2 lines will display value of x first for the first console.log before incrementing the value of x
+//the second line (20) will display the incremented value of x from line (19)}
+
+
+
+console.log(--x);//same as line 17
+
+//below lines uses the same algorithm as lines 19 and 20
+console.log(x--);
+console.log(x);
