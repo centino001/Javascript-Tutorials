f6ca8681b396eeae4d0d34b3037ae70570574ac6
centino001 <80419369+centino001@users.noreply.github.com>
2021-12-29 14:52:04 +0100

    added new file

---
+++ Arithmetic.js
@@ -0,0 +1,11 @@
+//Hello, this file shows how to get around with Arithmetic operators
+//these operators are; '+' '-' '*' '/' '%' 
+//(addition, subtraction, multiplication, division and modulus division respectively)
+
+let x=3;//let is used to declare varaibles in javascript
+let y=16;
+
+//you could also declare the 2 variables using a single let keyword:::::
+//let x=4, y=15; :::::uncomment this line and test the code to be sure it's the same as the former
+
+console.log(x/y);
