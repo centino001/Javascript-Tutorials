c94e6c82fef40acc0aa88f21503886fea8d262c9
centino001 <centino001@gmail.com>
2022-01-04 00:39:07 +0100

    new

---
+++ forof.js
@@ -0,0 +1,6 @@
+// this is similar to the previous loop (for-in) buh this is the best way to interate through an array
+
+const rooms = ['bed', 'chair', 'table', 'mat'];
+
+for (let room of rooms)
+console.log(room);
---
+++ max.js
@@ -0,0 +1,17 @@
+//this is an exercise that will output the maximum value of 2 numbers
+
+// let number =max(3,4);
+// console.log(number);
+
+// function max(x,y){
+//   return  (x>y) ? x:y;
+// }
+
+
+
+///this one output true if a photo is landscape
+console.log(isLandscape(2000, 400));
+
+function isLandscape(width, height){
+    return(width>height);
+}
