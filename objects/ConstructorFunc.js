660ae9e6b68d6d928b79ceee8d7c92ee96c69cf4
centino001 <centino001@gmail.com>
2022-01-05 23:49:01 +0100

    readme

---
+++ objects/ConstructorFunc.js
@@ -0,0 +1,17 @@
+//this type of function creates a new empty object and uses the "this" keyword to point to the newly created object
+//it uses pascal naming notation "CircleFunction"
+
+
+
+
+// function Circle(radius){
+//     this.radius=radius;
+//     this.draw = function(){
+//         console.log('draw');
+//     }
+// }
+
+// const circle= new Circle(2);
+// console.log(circle);
+
+ 
